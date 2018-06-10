4565d37ac3292ac6c551e53757eef511bebf1d15
赤木 <chimu@anybeen.net>
2018-06-10 21:59:06 +0800

    补充淘宝商品规则

--- src/autoload.php
+++ src/autoload.php
@@ -6,8 +6,6 @@ function ezLoader($class)
     $class = str_replace('Ezspider\\','',$class);
     $path = str_replace('\\', DIRECTORY_SEPARATOR, $class);
     $file = __DIR__ . DIRECTORY_SEPARATOR . $path . '.php';
-    echo $class."\t";
-    echo $file."\n";
     if (file_exists($file)) {
         require_once $file;
     }
